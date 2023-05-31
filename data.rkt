15cf8f9dcf097efe96ff250b799129b8048c8874
Antigen-1 <zhanghao@antigen.top>
2023-05-31 17:51:22 +0800

    improve the performance

--- data.rkt
+++ data.rkt
@@ -4,21 +4,19 @@
 
 ;;--------------------------
 ;;the vertical barrier and generic interfaces
-(define table (make-hash))
+(define table (make-hasheq))
 
 (define (tag? o) (and (symbol? o) (symbol-interned? o)))
-(define (already-has? t) (let/cc ret (hash-for-each table (lambda (key val) (cond ((eq? (car key) t) (ret #t))))) #f))
 (define (has-key? p) (hash-has-key? table p))
 
 (define/contract (install type contract ->pict . rest) ;;install a new datatype
-  (->i ((type (and/c tag? (not/c already-has?))) (contract contract?) (->pict (contract) (-> contract pict?)))
-       #:rest (rest (contract) (listof (cons/c tag? (-> contract any/c ... any))))
+  (->i ((type (and/c tag? (not/c has-key?))) (contract contract?) (->pict (contract) (-> contract pict?)))
+       #:rest (rest (contract) (listof (cons/c (and/c tag? (not/c '->pict)) (-> contract any/c ... any))))
        any)
-  (hash-set! table (cons type '->pict) ->pict)
-  (map (lambda (pair) (hash-set! table (cons type (car pair)) (cdr pair))) rest))
+  (hash-set! table type (make-hasheq (cons (cons '->pict ->pict) rest))))
 (define/contract (index type op)
-  (->i ((type tag?) (op tag?)) #:pre (type op) (has-key? (cons type op)) any)
-  (hash-ref table (cons type op)))
+  (->i ((type (and/c tag? has-key?)) (op tag?)) #:pre (type op) (hash-has-key? (hash-ref table type) op) any)
+  (hash-ref (hash-ref table type) op))
 
 (struct tagged-object (tag content))
 
